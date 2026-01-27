ea627f173d0128b80218389c6cfbca44be66636c
joejmendezsr <joejmendez@gmail.com>
2026-01-26 23:12:14 -0800

    Update app.py

--- mywebsite/app.py
+++ mywebsite/app.py
@@ -12,7 +12,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 @login_manager.user_loader
 def load_user(user_id):
-return User.query.get(int(user_id))
+     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
 id = db.Column(db.Integer, primary_key=True)
