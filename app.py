5c14ea95a741f8b99cc1b1aef47ea34e16b5c929
joejmendezsr <joejmendez@gmail.com>
2026-01-26 22:11:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,26 @@ class User(db.Model, UserMixin):
 @app.route("/")
 def hello():
     return "Hello, Joe!"
+from flask import request, redirect, url_for, render_template_string, flash
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(username=username, password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Account created! You can now log in.")
+        return redirect(url_for("login"))
+    return render_template_string("""
+    <form method="post">
+        Username: <input name="username"><br>
+        Password: <input name="password" type="password"><br>
+        <input type="submit" value="Register">
+    </form>
+    """)
 
 if __name__ == "__main__":
     with app.app_context():
