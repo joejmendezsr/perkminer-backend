f63073925fd845fcb06105a5977a006964e6114e
joejmendezsr <joejmendez@gmail.com>
2026-01-26 21:46:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'replace-this-with-a-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 @app.route("/")
 def hello():
