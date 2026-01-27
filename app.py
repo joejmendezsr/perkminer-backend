62812aa0621f7db321b2f892c5ac6650779e6dfc
joejmendezsr <joejmendez@gmail.com>
2026-01-26 21:48:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -14,3 +19,4 @@ login_manager = LoginManager(app)
 @app.route("/")
 def hello():
     return "Hello, Joe!"
+
