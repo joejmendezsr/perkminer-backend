6dad782921025cdb2dda0f9c115cecc86c676dae
joejmendezsr <joejmendez@gmail.com>
2026-01-26 22:10:32 -0800

    Update app.py

--- mywebsite/app.py
+++ mywebsite/app.py
@@ -1,7 +1,46 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'replace-this-with-a-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
 @app.route("/")
 def hello():
     return "Hello, Joe!"
+from flask import request, redirect, url_for, render_template_string, flash
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(username=username, password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Account created! You can now log in.")
+        return redirect(url_for("login"))
+    return render_template_string("""
+    <form method="post">
+        Username: <input name="username"><br>
+        Password: <input name="password" type="password"><br>
+        <input type="submit" value="Register">
+    </form>
+    """)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
