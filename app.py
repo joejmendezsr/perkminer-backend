96a1e7e3c9633f7939eb2ef4e42838cd6d0f6178
joejmendezsr <joejmendez@gmail.com>
2026-01-26 21:59:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
 from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'replace-this-with-a-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
 @app.route("/")
 def hello():
