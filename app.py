03a7eae7e79aaf1f606b822e5c76fa75da6684ac
joejmendezsr <joejmendez@gmail.com>
2026-01-26 22:46:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
