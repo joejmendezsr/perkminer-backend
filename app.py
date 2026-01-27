fe99665f6c0ada1deddd86ad846e3427a6c1eee5
joejmendezsr <joejmendez@gmail.com>
2026-01-26 23:17:24 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,78 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template_string, flash
 
-app = Flask(name)
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'replace-this-with-a-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
 @login_manager.user_loader
 def load_user(user_id):
-     return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-id = db.Column(db.Integer, primary_key=True)
-username = db.Column(db.String(150), unique=True, nullable=False)
-password = db.Column(db.String(60), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
 @app.route("/")
 def hello():
-return "Hello, Joe!"
-from flask import request, redirect, url_for, render_template_string, flash
+    return "Hello, Joe!"
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-if request.method == "POST":
-username = request.form.get("username")
-password = request.form.get("password")
-if not username or not password:
-flash("Username and password cannot be empty.")
-return redirect(url_for("register"))
-hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
-new_user = User(username=username, password=hashed_pw)
-db.session.add(new_user)
-db.session.commit()
-flash("Account created! You can now log in.")
-return redirect(url_for("login"))
-return render_template_string("""
-<form method="post">
-Username: <input name="username"><br>
-Password: <input name="password" type="password"><br>
-<input type="submit" value="Register">
-</form>
-""")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if not username or not password:
+            flash("Username and password cannot be empty.")
+            return redirect(url_for("register"))
+        hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(username=username, password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Account created! You can now log in.")
+        return redirect(url_for("login"))
+    return render_template_string("""
+        <form method="post">
+            Username: <input name="username"><br>
+            Password: <input name="password" type="password"><br>
+            <input type="submit" value="Register">
+        </form>
+    """)
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
-message = ""
-if request.method == "POST":
-username = request.form.get("username")
-password = request.form.get("password")
-user = User.query.filter_by(username=username).first()
-if user and bcrypt.check_password_hash(user.password, password):
-login_user(user)
-return redirect(url_for("dashboard"))
-else:
-message = "Login failed. Check username and password."
-return render_template_string("""
-<form method="post">
-Username: <input name="username"><br>
-Password: <input name="password" type="password"><br>
-<input type="submit" value="Login">
-<div style='color:red;'>{{message}}</div>
-</form>
-""", message=message)
+    message = ""
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("dashboard"))
+        else:
+            message = "Login failed. Check username and password."
+    return render_template_string("""
+        <form method="post">
+            Username: <input name="username"><br>
+            Password: <input name="password" type="password"><br>
+            <input type="submit" value="Login">
+            <div style='color:red;'>{{message}}</div>
+        </form>
+    """, message=message)
+
 @app.route("/dashboard")
 @login_required
 def dashboard():
-return f"Welcome, {current_user.username}! This is your dashboard."
-if name == "main":
-with app.app_context():
-db.create_all()
-app.run(debug=True)
+    return f"Welcome, {current_user.username}! This is your dashboard."
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
